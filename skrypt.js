d15983e010e8668a98a14a276d9d98fe8159b4eb
danielstrzelecki99 <danielstrzelecki99@wp.pl>
2020-12-29 15:49:24 +0100

    Program on classes, didn't work

--- skrypt.js
+++ skrypt.js
@@ -3,24 +3,35 @@ const ctx = can.getContext("2d");
 const cw = can.width = 1200;
 const ch = can.height = 800;
 
+window.onload = function(){
+	const playergraph = new Image();
+	playergraph.src="img/gracz.png";
+}
+
 function printmap(){
-	
 	let minion = new Image();
 	minion.src="img/minion.png";
 	minion.onload = function(){
-		ctx.drawImage(this,80,70,70,70);
+	ctx.drawImage(this,80,70,70,70);
 	}
 }
 
 class Minion{
-	let positionX;
-	let positionY;
 	constructor(){
 		let hp = 1;
 	}
 }
 
 class Player{
+	constructor(){
+		let posX = cw/2;
+		let posY = ch/2;
+	}
+	render(){
+		ctx.drawImage(playergraph, 100, 100, 100, 100);
+	}
 }
 
 printmap();
+const player = new Player();
+player.render();
